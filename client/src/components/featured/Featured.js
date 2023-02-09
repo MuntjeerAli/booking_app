562e111853710c02b2db621d9de93f23514d6136
MuntjeerAli <muntjeersayyad@gmail.com>
2023-02-09 19:34:06 +0530

    home component completed

---
+++ client/src/components/featured/Featured.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import "./featured.css";
+
+const featured = () => {
+  return (
+    <div className='featured'>
+      <div className='featuredItem'>
+        <img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.britannica.com%2Fplace%2FMumbai&psig=AOvVaw3LV4hr_p-fBUKk2IoxLP-p&ust=1675946615902000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKiLy-r5hf0CFQAAAAAdAAAAABAJ' alt='' className='featuredImg'/>
+        <div className='featuredTitles'>
+            <h1>Mumbai</h1>
+            <h2>121 properties</h2>
+        </div>
+      </div>
+      <div className='featuredItem'>
+        <img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fmittalbuilders.com%2Freasons-to-settle-down-in-pune%2F&psig=AOvVaw1HRon_YFg17DLX3SZZbAnN&ust=1675947278288000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCOj_5Kn8hf0CFQAAAAAdAAAAABAD' alt='' className='featuredImg'/>
+        <div className='featuredTitles'>
+            <h1>Pune</h1>
+            <h2>91 properties</h2>
+        </div>
+      </div>
+      <div className='featuredItem'>
+        <img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.karnatakatourism.org%2Fparks-in-bengaluru%2F&psig=AOvVaw1RHZ0bW_BCZMGUDWbGsPtV&ust=1675947190280000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKDQ4vr7hf0CFQAAAAAdAAAAABAJ' alt='' className='featuredImg'/>
+        <div className='featuredTitles'>
+            <h1>Bangalore</h1>
+            <h2>102 properties</h2>
+        </div>
+      </div>
+      <div className='featuredItem'>
+        <img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tripadvisor.in%2FTourism-g297586-Hyderabad_Hyderabad_District_Telangana-Vacations.html&psig=AOvVaw1EDmEJDRNB3dlKDnuvx7S8&ust=1675947151534000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCMDoyuf7hf0CFQAAAAAdAAAAABAE' alt='' className='featuredImg'/>
+        <div className='featuredTitles'>
+            <h1>Hyderabad</h1>
+            <h2>89 properties</h2>
+        </div>
+      </div>
+      <div className='featuredItem'>
+        <img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.magicbricks.com%2Fblog%2Fposh-area-in-chennai%2F119429.html&psig=AOvVaw0y5qxO7fvnuyDgAx8DgWU6&ust=1675947089227000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLDd5Mn7hf0CFQAAAAAdAAAAABAE' alt='' className='featuredImg'/>
+        <div className='featuredTitles'>
+            <h1>Chennai</h1>
+            <h2>81 properties</h2>
+        </div>
+      </div>
+      <div className='featuredItem'>
+        <img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FJaipur&psig=AOvVaw2h7sRYWGw8iKd0k5Fgwo46&ust=1675947049856000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCOC1pbf7hf0CFQAAAAAdAAAAABAE' alt='' className='featuredImg'/>
+        <div className='featuredTitles'>
+            <h1>Jaipur</h1>
+            <h2>79 properties</h2>
+        </div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default featured
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import Navbar from '../../components/navbar/Navbar.js';
 import Header from '../../components/header/Header.js';
+import Featured from '../../components/featured/Featured.js';
 import "./home.css"
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
-      <Header />
+      <Header/>
+      <div className='homeContainer'>
+        <Featured/>
+      </div>
     </div>
+    
   )
 }
 
